98f119677b9fed775c5f7a6242a757409c76c7f4
Kartik Neti <kartikprajak@gmail.com>
2023-11-06 22:37:23 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name":"MyStore",
+        "items":[
+            {
+                "name":"chair",
+                "price": 50.99
+            }
+        ]
+    }
+]
+
+
+@app.get('/store')
+def get_stores():
+    return {"stores":stores}
+
+@app.post('/store')
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name":request_data["name"], "items":request_data["items"]}
+    stores.append(new_store)
+    return new_store, 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
